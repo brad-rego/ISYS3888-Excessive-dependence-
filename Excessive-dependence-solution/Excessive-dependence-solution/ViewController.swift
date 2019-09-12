482fd900c8a42b65319b87b160f9bb271a053a71
Bradley <breg2851@uni.sydney.edu.au>
2019-09-12 16:22:33 +1000

    Inital work

---
+++ Excessive-dependence-solution/Excessive-dependence-solution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Excessive-dependence-solution
+//
+//  Created by Bradley Rego on 12/9/19.
+//  Copyright © 2019 The Survivors squad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Excessive-dependence-solution/Excessive-dependence-solutionTests/Excessive_dependence_solutionTests.swift
@@ -0,0 +1,34 @@
+//
+//  Excessive_dependence_solutionTests.swift
+//  Excessive-dependence-solutionTests
+//
+//  Created by Bradley Rego on 12/9/19.
+//  Copyright © 2019 The Survivors squad. All rights reserved.
+//
+
+import XCTest
+@testable import Excessive_dependence_solution
+
+class Excessive_dependence_solutionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
